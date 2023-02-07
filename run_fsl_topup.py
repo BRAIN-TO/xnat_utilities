5899572f7d82af61d719f2edacc0c097fb543299
Yuexin Xi <845127818virna@gmail.com>
2023-02-07 16:11:19 -0500

    Added FUGUE option and workflow option

--- run_fsl_topup.py
+++ run_fsl_topup.py
@@ -1,7 +1,15 @@
+from builtins import range
+import nipype.interfaces.io as nio  # Data i/o
+import nipype.interfaces.fsl as fsl
+from nipype.interfaces.fsl.epi import TOPUP, ApplyTOPUP
+from nipype.interfaces.fsl.preprocess import FUGUE
+import nipype.pipeline.engine as pe  # pypeline engine
+from nipype.interfaces.base import Bunch
 from sys import argv
-from nipype.interfaces.fsl import TOPUP, ApplyTOPUP
+import nipype.interfaces.io as nio
 import json
 import subprocess
+import os
 
 script, file1, file2 = argv
 
@@ -36,6 +44,51 @@ f = open('acq_param.txt', 'a')
 f.write(f"0 -1 0 {c4}\n0 1 0 {c4}")
 f.close
 
+"""
+#fsl topup
+topup = pe.Node(interface=fsl.TOPUP(), name="topup")
+topup.inputs.in_file = "both_b0.nii.gz"
+topup.inputs.encoding_file = "acq_param.txt"
+topup.inputs.out_field = "fieldmap_Hz.nii.gz"
+
+#fslmath to convert to radian-change
+fslmaths = pe.Node(interface=fsl.BinaryMaths(), name="fslmaths")
+fslmaths.inputs.operation = 'mul'
+fslmaths.inputs.operand_value = 5
+fslmaths.inputs.out_file = "fieldmap_radian.nii.gz"
+
+#fsl fugue
+fugue = pe.Node(interface=fsl.FUGUE(), name="fugue")
+fugue.inputs.in_file = image1
+fugue.inputs.dwell_time = value1
+fugue.inputs.unwarped_file = f"{image1}_corrected.nii.gz"
+fugue.inputs.unwarp_direction = "y-"
+fugue.inputs.save_shift = True
+
+#connect the outputs to the inputs
+preprocessing = pe.Workflow(name="preprocesing")
+preprocessing.connect(topup, "out_field", fslmaths, "in_file")
+preprocessing.connect(fslmaths, "out_file", fugue, "fmap_in_file")
+
+#datasource
+datasource = pe.Node(
+    interface=nio.DataGrabber(outfields=['func']), name='datasource')
+datasource.inputs.base_directory = "/home/yuexin/Documents/topup/"
+datasource.inputs.sort_filelist = True
+datasource.inputs.template = 'both_b0.nii.gz'
+
+#connect preprocessing to datasource
+preprocessing.connect(datasource, 'func', topup, 'in_file')
+
+datasink = pe.Node(interface=nio.DataSink(), name="datasink")
+datasink.inputs.base_directory = os.path.abspath('/home/yuexin/Documents/topup/output/')
+preprocessing.connect(fugue, 'unwarped_file', datasink, 'contrasts.@T')
+
+
+preprocessing.config['execution']['job_finished_timeout'] = 60
+preprocessing.run()
+"""
+
 #fsl topup
 topup = TOPUP()
 topup.inputs.in_file = "both_b0.nii.gz"
@@ -44,9 +97,7 @@ topup.inputs.out_field = "fieldmap_Hz.nii.gz"
 topup.inputs.out_base = "my_topup"
 res = topup.run()
 
-#fslmaths to convert to radian-change
-#subprocess.run(["fslmaths", "fieldmap_Hz", "-mul", "6.28", "fieldmap_radian"])
-
+"""
 #fsl applytopup
 applytopup = ApplyTOPUP()
 applytopup.inputs.in_files = [image1, image2]
@@ -54,3 +105,17 @@ applytopup.inputs.encoding_file = "acq_param.txt"
 applytopup.inputs.in_topup_fieldcoef = "my_topup_fieldcoef.nii.gz"
 applytopup.inputs.in_topup_movpar = "my_topup_movpar.txt"
 res = applytopup.run()
+"""
+
+#fslmath to convert to radian-change
+subprocess.run(["fslmaths", "fieldmap_Hz", "-mul", "6.28", "fieldmap_radian"])
+
+#fsl fugue
+fugue = FUGUE()
+fugue.inputs.in_file = image1
+fugue.inputs.dwell_time = value1
+fugue.inputs.fmap_in_file = "fieldmap_radian.nii.gz"
+fugue.inputs.unwarped_file = f"{image1}_corrected.nii.gz"
+fugue.inputs.unwarp_direction = "y-"
+fugue.inputs.save_shift = True
+res = fugue.run()
